bb8168b389ac207318d5e1c9639b27d0be07e2e4
Sweetsan <coderintheworldhack100@gmail.com>
2026-04-12 13:00:28 +0530

    script file is arranged

--- practice.js
+++ practice.js
@@ -1,97 +1,91 @@
-let a=10;
-let b=20;
-console.log(a+b);
-console.log(a*b);
+let a = 10;
+let b = 20;
+console.log(a + b);
+console.log(a * b);
 // post increment a++ and a--
-b=a++;
+b = a++;
 console.log(b);
 console.log(a);
 // pre increment ++a and --a
-a=11;
-b=13;
-b=++a;
+a = 11;
+b = 13;
+b = ++a;
 console.log(b);
 // data types
-console.log(typeof(10));// number
-console.log(typeof(true));//boolean
-console.log(typeof("Apple"));//string
-console.log(typeof(null));// object
-console.log(typeof(undefined))// undefine
-console.log(typeof([Array])) // object
+console.log(typeof 10); // number
+console.log(typeof true); //boolean
+console.log(typeof "Apple"); //string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefine
+console.log(typeof [Array]); // object
 // functions
-function hi(){
-    console.log("welcome to js world");
+function hi() {
+  console.log("welcome to js world");
 }
-hi()
-function iphone(){
-    console.log("This is an iphone")
+hi();
+function iphone() {
+  console.log("This is an iphone");
 }
-function redmi(){
-    console.log("This is a Redmi")
+function redmi() {
+  console.log("This is a Redmi");
 }
-function oppo(){
-    console.log("This is an oppo")
+function oppo() {
+  console.log("This is an oppo");
 }
-iphone()
-redmi()
-oppo()
+iphone();
+redmi();
+oppo();
 
-a=10;
-b=12;
-function add(){
-    console.log(a+b)
+a = 10;
+b = 12;
+function add() {
+  console.log(a + b);
 }
-add()
+add();
 
-var factor="Kamal";
-var fplayer="Dhoni";
-var fmovie="Anbe sivam";
-function favorite(){
-    console.log("favourite actor is :" +" "+ factor);
-    console.log("favourite player is :"+" "+ fplayer);
-    console.log("favourite movie name is :"+ " " + fmovie);
+var factor = "Kamal";
+var fplayer = "Dhoni";
+var fmovie = "Anbe sivam";
+function favorite() {
+  console.log("favourite actor is :" + " " + factor);
+  console.log("favourite player is :" + " " + fplayer);
+  console.log("favourite movie name is :" + " " + fmovie);
 }
-favorite()
+favorite();
 
-function area(length,breadth){
-    console.log("Area is"+" "+length*breadth);
+function area(length, breadth) {
+  console.log("Area is" + " " + length * breadth);
 }
-area(10,12.34)
+area(10, 12.34);
 // return keyword
-function myName(){
-    return "Sweetsan"
+function myName() {
+  return "Sweetsan";
 }
-var A=myName();
+var A = myName();
 console.log(A);
 
-
-function add(a,b){
-    return(a+b);
+function add(a, b) {
+  return a + b;
 }
-var total=add(10,20);
+var total = add(10, 20);
 console.log(total);
 
 // if else condition
-let homework=true;
-if(homework){
-    console.log(`Great job`);
-}
-else{
-    console.log(`Finish your homework before playing`);
+let homework = true;
+if (homework) {
+  console.log(`Great job`);
+} else {
+  console.log(`Finish your homework before playing`);
 }
 
-let season="spring";
-if(season == "spring"){
-    console.log(`Enjoy the blooming flower`);
+let season = "spring";
+if (season == "spring") {
+  console.log(`Enjoy the blooming flower`);
 }
-if(season == "summer"){
-console.log(`Have fun in the sun`);
+if (season == "summer") {
+  console.log(`Have fun in the sun`);
 }
-        
-if(season == "autumn"||"fall"){
-console.log(`Admire colorful leaves`);
+
+if (season == "autumn" || "fall") {
+  console.log(`Admire colorful leaves`);
 }
-         
-
-
-
